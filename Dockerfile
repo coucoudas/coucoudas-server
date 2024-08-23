659a03cfca1ef4078229e30ed4962edacabe976f
yu-heejin <jkgt0521@naver.com>
2024-08-23 23:13:57 +0900

    refactor: Router 분리

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,6 @@
 FROM python:3.9
 
 COPY ./requirements.txt ./requirements.txt
-
 RUN pip install --no-cache-dir --upgrade -r ./requirements.txt
-
 COPY . .
-
-CMD ["uvicorn", "app.api:app", "--host", "0.0.0.0", "--port", "8000"]
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
--- app/api/board_api.py
+++ app/api/board_api.py
@@ -1,19 +1,17 @@
-from fastapi import FastAPI, Depends
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from .dto import CreateBoardRequest, UpdateBoardRequest
-from .database import connect_database
-from .model import insert_board, find_boards, find_board, update_board, delete_board
+from app.dto import CreateBoardRequest, UpdateBoardRequest
+from app.database import connect_database
+from app.model import insert_board, find_boards, find_board, update_board, delete_board
 
-app = FastAPI(docs_url='/api/docs', redoc_url='/api/redoc')
-
-# root page
-@app.get("/")
-def hello_world():
-    return { "status": 200, "message": "hello world" }
+board_router = APIRouter(
+    prefix = "/boards",
+    tags = ["board"]
+)
 
 # Create Board
-@app.post(path = "/boards", description = "게시판 생성 API")
+@board_router.post(path = "/", description = "게시판 생성 API")
 def create_board(
     board_request: CreateBoardRequest, 
     db: Session = Depends(connect_database)
@@ -22,19 +20,19 @@ def create_board(
     return { "status": 201, "message": f"/boards/{result}"}
 
 # Read Board List
-@app.get(path = "/boards", description = "게시판 전체 조회 API")
+@board_router.get(path = "/", description = "게시판 전체 조회 API")
 def get_boards(db: Session = Depends(connect_database)):
     results = find_boards(db)
     return { "status": 200, "message": "SUCCESS", "data": results }
 
 # Read Board
-@app.get(path = "/boards/{id}", description = "게시판 상세 조회 API")
+@board_router.get(path = "/{id}", description = "게시판 상세 조회 API")
 def get_board(id: int, db: Session = Depends(connect_database)):
     result = find_board(id, db)
     return { "status": 200, "message": "SUCCESS", "data": result }
 
 # Update Board
-@app.patch(path = "/boards/{id}", description = "게시판 수정 API")
+@board_router.patch(path = "/{id}", description = "게시판 수정 API")
 def edit_board(
     id: int,
     board_request: UpdateBoardRequest,
@@ -44,7 +42,7 @@ def edit_board(
     return { "status": 200, "message": "SUCCESS" }
 
 # Delete Board
-@app.delete(path = "/boards/{id}", description = "게시판 삭제 API")
+@board_router.delete(path = "/{id}", description = "게시판 삭제 API")
 def remove_board(id: int, db: Session = Depends(connect_database)):
     delete_board(id, db)
     return { "status": 200, "message": "SUCCESS" }
