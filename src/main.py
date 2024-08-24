263aa34ed5b6cbfe9e39b813268df4a17a143579
yu-heejin <jkgt0521@naver.com>
2024-08-24 16:44:55 +0900

    fix: swagger render 오류 수정

--- src/main.py
+++ src/main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 from src.api.board_api import board_router
 
-app = FastAPI(docs_url="/api/docs", redoc_url="/api/redoc")
+app = FastAPI(
+    docs_url="/api/docs", 
+    openapi_url="/api/openapi.json",
+    redoc_url=None
+)
 
 app.include_router(board_router, prefix = "/api")
