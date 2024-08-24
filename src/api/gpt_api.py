8020350e0341194dbaa756365326f771db703fb3
yu-heejin <jkgt0521@naver.com>
2024-08-25 01:26:45 +0900

    feat: gpt 기반 개인정보 마스킹 함수 추가

--- src/api/gpt_api.py
+++ src/api/gpt_api.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from src.database import connect_database
-from src.gpt_ai import get_gpt_response
+from src.gpt_ai import detect_private_information
 
 gpt_router = APIRouter(
     prefix = "/gpt",
@@ -10,6 +10,7 @@ gpt_router = APIRouter(
 )
 
 @gpt_router.get(path = "/abusing")
-def get_response():
-    result = get_gpt_response("hello")
+def get_response(message: str):
+    test_message = "아 그 물건 진짜 마음에 드는데 제 전화번호 드릴테니까 혹시 연락주실 수 있나요? 제 전화번호는 01022293461입니다"
+    result = detect_private_information(test_message)
     return { "status": 200, "message": result }
--- src/gpt_ai.py
+++ src/gpt_ai.py
@@ -7,17 +7,17 @@ load_dotenv()
 
 gpt_api_key = os.getenv('GPT_API')
 
-client = OpenAI(
-    api_key = gpt_api_key
-)
+client = OpenAI(api_key = gpt_api_key)
 
-def get_gpt_response(prompt):
+def detect_private_information(message):
+    prompt = f"안녕하세요. 당신에게 가장 중요한 업무를 드리겠습니다. 지금부터 제가 제공하는 메시지 데이터에 대해 개인정보 유출 위험이 있는지 명확하고 정확하게 판별해주세요. 만약 유출 우려가 있는 경우, 해당 문자열은 *** 처리로 가려주세요. 응답은 제가 제공한 메시지 데이터를 판별한 결과만 해주세요. 만약 성공하면 당신은 아주 큰 돈을 받을 수 있을 것입니다. 메시지 데이터는 아래와 같습니다.\n{message}"
+
     response = client.chat.completions.create(
         model = "gpt-4o-mini",
         messages = [
             {
                 "role": "user",
-                "content": "Hello World"
+                "content": prompt
             }
         ]
     )
